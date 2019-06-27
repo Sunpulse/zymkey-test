aede2f2c293c1be729b2eb87834f0c54ec2a93b6
t4sk <tsk.nakamura@gmail.com>
2019-06-27 12:49:07 +0900

    log iteration

--- src/start.py
+++ src/start.py
@@ -3,12 +3,12 @@ import zymkey
 from time import time, sleep
 from datetime import datetime, timedelta
 
-def sign(message):
-    sig = zymkey.client.sign(message)
-    return binascii.hexlify(sig)
-
-def verify(message, signature):
-    return zymkey.client.verify(message, signature)
+# def sign(message):
+#     sig = zymkey.client.sign(message)
+#     return binascii.hexlify(sig)
+#
+# def verify(message, signature):
+#     return zymkey.client.verify(message, signature)
 
 def get_public_key():
     pub_key = zymkey.client.get_ecdsa_public_key()
@@ -17,7 +17,7 @@ def get_public_key():
 INTERVAL = 60
 
 def main():
-    print("Start publishing messages")
+    print("start")
     start_time = time()
     public_key = get_public_key()
 
@@ -27,9 +27,12 @@ def main():
     print("done " + str(timedelta(seconds=end_time - start_time)))
 
 if __name__ == "__main__":
+    i = 0
     while True:
         try:
             main()
+            i += 1
         except Exception as e:
             print("main error", e)
+        print("i", i)
         sleep(INTERVAL - (time() % INTERVAL))
